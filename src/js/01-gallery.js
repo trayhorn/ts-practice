e37d4451ff5275de31102750d269957d7bf29127
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-02-25 20:01:44 +0200

    gallery renders, openModal with lib

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+
+const markUp = (items) => {
+  return items.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`
+  }).join('')
+}
+
+const handleClick = e => {
+  e.preventDefault();
+  if (e.target.className === 'gallery__image') {
+    const modal = basicLightbox.create(`
+      <img width="1400" height="900" src="${e.target.dataset.source}">
+    `, {
+      onShow: (instance) => {
+        document.addEventListener('keydown', e => {
+          if (e.code === 'Escape') {
+            instance.close();
+          }
+        })
+      },
+    })
+    modal.show();
+  }
+}
+
+galleryEl.insertAdjacentHTML('beforeend', markUp(galleryItems));
+
+galleryEl.addEventListener('click', handleClick);
